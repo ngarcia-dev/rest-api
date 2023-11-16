00139c003152bda51b9044c0913b11afef1e1c84
ngarcia <garcianahuel77@gmail.com>
2023-11-16 11:03:26 -0300

    If you do not specify a role when registering, the USER role will be saved by default

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -24,13 +24,12 @@ export const register = async (req, res) => {
       roles,
     });
 
-    if (!roles) {
-      const role = await Role.findOne({ name: "user" });
-      newUser.roles = [role._id];
-      return;
+    if (roles) {
+      const foundRoles = await Role.find({ name: { $in: roles } });
+      newUser.roles = foundRoles.map((role) => role._id);
     }
-    const foundRoles = await Role.find({ name: { $in: roles } });
-    newUser.roles = foundRoles.map((role) => role._id);
+    const role = await Role.findOne({ name: "user" });
+    newUser.roles = [role._id];
 
     // crea un usuario y lo guarda en la base de datos
     const userSaved = await newUser.save();
